5b4ca5deaa801a59385a9bede8a2ddd0c3844b91
Kish Siva <rodes88@msn.com>
2019-04-22 22:26:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -15,6 +17,7 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
+    binding.pry
     BRANDS << brand 
   end
   
